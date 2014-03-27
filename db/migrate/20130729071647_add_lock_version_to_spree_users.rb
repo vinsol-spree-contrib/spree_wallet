df79eb8757c5f5dfd7ab098d74ff66f6083a3952
nishant-cyro <nishant.tuteja@vinsol.com>
2014-03-27 13:17:22 +0530

    add condition of lock_version in spree_users

--- db/migrate/20130729071647_add_lock_version_to_spree_users.rb
+++ db/migrate/20130729071647_add_lock_version_to_spree_users.rb
@@ -1,5 +1,7 @@
 class AddLockVersionToSpreeUsers < ActiveRecord::Migration
   def change
-    add_column :spree_users, :lock_version, :integer, :default => 0
+    if Spree::User.column_names.include? "lock_version"
+      add_column :spree_users, :lock_version, :integer, :default => 0
+    end
   end
 end
